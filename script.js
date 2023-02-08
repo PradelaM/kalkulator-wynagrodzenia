7b7ccd86e501c965221038cc8ad2f7f3da49487f
Maria <marusn@o2.pl>
2023-02-08 17:22:10 +0100

    first

---
+++ script.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('.quiz-box');
+const answers = Array.from(document.querySelectorAll('.answer'));
+const allQuestions = document.querySelectorAll('.question');
+const modal = document.querySelector('.modal');
+const modalInfo = modal.querySelector('p');
+const modalBtn = modal.querySelector('.close-modal');
+
+const handleQuiz = (e) => {
+	e.preventDefault();
+	const checkedAnswers = answers.filter(answer => answer.checked);
+	
+	const isTrue = checkedAnswers.every(answer => answer.value === 'true');
+	const allChecked = checkedAnswers.length === allQuestions.length;
+
+	console.log({checkedAnswers,isTrue,allQuestions,allChecked})
+	if (!allChecked) {
+		modal.classList.add('modal-active');
+		modalInfo.textContent = 'zaznacz wszystko baranie';
+        return
+	}
+
+	console.log({allChecked})
+	checkedAnswers.forEach(answer => {
+		const checkIfcorrect = answer.value === 'true';
+		const answerBox = answer.closest('.answer-box');
+
+		if (checkIfcorrect) {
+			answerBox.classList.add('correct');
+			answerBox.classList.remove('incorrect');
+		} else {
+			answerBox.classList.add('incorrect');
+			answerBox.classList.remove('correct');
+		}
+	});
+
+    if(isTrue && allChecked){
+    modal.classList.add('modal-active');
+    modalInfo.textContent = 'Brawooo Krisuu'
+}
+else{
+    modal.classList.add('modal-active')
+    modalInfo.textContent = 'A żeś zdupił'
+}
+
+}
+
+const closeModal=()=> {
+    modal.classList.remove('modal-active')
+}
+
+modalBtn.addEventListener('click', closeModal);
+form.addEventListener('submit', handleQuiz);
